26147af933aa9032258fa53a2d7aff14b09807f0
Mengxiao Lin <linmx0130@gmail.com>
2018-04-07 14:46:34 +0800

    update max margin loss: add regularization and margin config

--- marginloss.py
+++ marginloss.py
@@ -13,7 +13,12 @@ from mxnet.io import NDArrayIter
 import logging
 
 class MarginLoss(mx.operator.CustomOp):
+    def __init__(self, margin=1, l2reg=0.001):
+        self.margin = float(margin)
+        self.l2reg = float(l2reg)
+
     def forward(self, is_train, req, in_data, out_data, aux):
+        # get margin loss
         x = in_data[0]
         label = in_data[1]
         ind = mx.nd.arange(0, x.shape[0])
@@ -22,8 +27,10 @@ class MarginLoss(mx.operator.CustomOp):
         xx = x.copy()
         xx[ind, label] = min_val -10
         mlp_max = mx.nd.max(xx, axis=1)
-        margin_loss = (1 - mlp_gt + mlp_max)
+        margin_loss = mlp_max - mlp_gt + self.margin
         margin_loss *= (margin_loss>0)
+        # get regularization loss
+        margin_loss += (x * x * self.l2reg).sum(axis=1)
         self.assign(out_data[0], req[0], margin_loss)
 
     def backward(self, req, out_grad, in_data, out_data, in_grad, aux):
@@ -43,12 +50,17 @@ class MarginLoss(mx.operator.CustomOp):
         grad[ind, label] = -1
         grad[ind, mlp_maxind] = 1
         grad *= mask
+
+        # derive reg loss
+        grad += x * self.l2reg * 2
         self.assign(in_grad[0], req[0], grad)
 
 @mx.operator.register("marginloss")
 class MarginLossProp(mx.operator.CustomOpProp):
-    def __init__(self):
+    def __init__(self, margin=1, l2reg=0.001):
         super(MarginLossProp, self).__init__(need_top_grad=False)
+        self.margin = margin
+        self.l2reg = l2reg
     def list_arguments(self):
         return ['data', 'label']
     def list_outputs(self):
@@ -62,10 +74,10 @@ class MarginLossProp(mx.operator.CustomOpProp):
         dtype = in_type[0]
         return [dtype, dtype], [dtype], []
     def create_operator(self, ctx, shapes, types):
-        return MarginLoss()
+        return MarginLoss(self.margin, self.l2reg)
 
-def max_margin_loss(data, label):
-    return mx.nd.Custom(data, label, op_type="marginloss")
+def max_margin_loss(data, label, margin=1, l2reg=0.001):
+    return mx.nd.Custom(data, label, margin=margin, l2reg=l2reg, op_type="marginloss")
 
 if __name__=="__main__":
     data = mx.symbol.Variable('data')
